a03bdfce27faf4d6343e0e0257940b4e2e2eb6c6
andxbes <andxbes@gmail.com>
2025-05-04 10:34:49 +0300

    554

--- src/containers/Favorites.js
+++ src/containers/Favorites.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useContext } from 'react';
+// import { useSelector } from 'react-redux';
 
 import FavoriteItem from '../components/Favorites/FavoriteItem';
 import './Products.css';
+import { ProductContext } from '../context/product-context';
 
 const Favorites = props => {
-  const favoriteProducts = useSelector(state =>
-    state.shop.products.filter(p => p.isFavorite)
-  );
+  const favoriteProducts = useContext(ProductContext).products.filter(p => p.isFavorite);
+
+  //   state =>
+  //   state.shop.products.filter(p => p.isFavorite)
+  // );
   let content = <p className="placeholder">Got no favorites yet!</p>;
   if (favoriteProducts.length > 0) {
     content = (
